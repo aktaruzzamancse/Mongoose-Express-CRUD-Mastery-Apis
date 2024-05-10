14e14ecfa2c00621b0f8e1202064815a2a939878
Mohammad Aktaruzzaman <aktaruzzamancse@gmail.com>
2024-05-10 10:31:21 +0600

    Schema Code refactor

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,20 +1,58 @@
 import { Schema, model, connect } from "mongoose";
-import { User } from "./user.interface";
+import { User, address, fullName } from "./user.interface";
+
+const fullNameSchema = new Schema<fullName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const addressSchema = new Schema<address>({
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
 const userSchema = new Schema<User>({
-  userId: { type: Number, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  fullName: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+  userId: {
+    type: Number,
+    required: true,
   },
-  age: { type: Number, required: true },
-  email: { type: String, required: true },
-  isActive: { type: Boolean, required: true },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: fullNameSchema,
+  age: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
   hobbies: [[String]],
-  address: {
-    street: { type: String, required: true },
-    city: { type: String, required: true },
-    country: { type: String, required: true },
-  },
+  address: addressSchema,
 });
