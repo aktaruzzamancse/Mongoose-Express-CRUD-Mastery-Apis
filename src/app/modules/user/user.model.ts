982c9fd2cab04d0db193b45d8a290e065be00702
Mohammad Aktaruzzaman <aktaruzzamancse@gmail.com>
2024-05-11 23:54:30 +0600

    Validation added

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -4,57 +4,64 @@ import { User, address, fullName } from "./user.interface";
 const fullNameSchema = new Schema<fullName>({
   firstName: {
     type: String,
-    required: true,
+    required: [true, "First Name is required"],
   },
   lastName: {
     type: String,
-    required: true,
+    required: [true, "Last Name is required"],
   },
 });
 
 const addressSchema = new Schema<address>({
   street: {
     type: String,
-    required: true,
+    required: [true, "Street adress is required"],
   },
   city: {
     type: String,
-    required: true,
+    required: [true, "City is required"],
   },
   country: {
     type: String,
-    required: true,
+    required: [true, "Country is required"],
   },
 });
 
 const userSchema = new Schema<User>({
   userId: {
     type: Number,
-    required: true,
+    required: [true, "User Id is required"],
   },
   username: {
     type: String,
-    required: true,
+    required: [true, "User Name is required"],
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Password is required"],
   },
-  fullName: fullNameSchema,
+  fullName: {
+    type: fullNameSchema,
+    required: [true, "FullName is required"],
+  },
   age: {
     type: Number,
-    required: true,
+    required: [true, "Age is required"],
   },
   email: {
     type: String,
-    required: true,
+    required: [true, "Email is required"],
   },
   isActive: {
     type: Boolean,
     required: true,
+    default: true,
   },
   hobbies: [String],
-  address: addressSchema,
+  address: {
+    type: addressSchema,
+    required: [true, "Address is required"],
+  },
 });
 
 export const UserModel = model<User>("User", userSchema);
